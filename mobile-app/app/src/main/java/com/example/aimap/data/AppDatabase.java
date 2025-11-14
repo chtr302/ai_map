5d11e953dabb4e90d4579efee6dfc2f7b1ad7b27
chtrn302 <traconghau0212@gmail.com>
2025-11-14 18:28:09 +0700

    Định nghĩa database với SQLite

--- mobile-app/app/src/main/java/com/example/aimap/data/AppDatabase.java
+++ mobile-app/app/src/main/java/com/example/aimap/data/AppDatabase.java
@@ -1,4 +1,37 @@
 package com.example.aimap.data;
 
-public class AppDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.aimap.data.dao.ChatMessageDao;
+import com.example.aimap.data.dao.SessionDao;
+
+@Database(
+        entities = {Session.class, ChatMessage.class},
+        version = 2,
+        exportSchema = false
+)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract SessionDao sessionDao();
+    public abstract ChatMessageDao chatMessageDao();
+
+    private static volatile AppDatabase INSTANCE;
+
+    public static AppDatabase getDatabase(final Context context){
+        if (INSTANCE == null){
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            AppDatabase.class,
+                            "chat_database"
+                    ).fallbackToDestructiveMigration().build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
